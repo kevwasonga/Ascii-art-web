ad82e9e7ec76e40ab2016afe59831ed2c95c0d8e
kevwasonga <kevinwasonga116@gmail.com>
2024-07-08 23:06:51 +0300

    comments

--- main.go
+++ main.go
@@ -11,25 +11,33 @@ import (
 )
 
 func main() {
-	// Serve the static HTML page
+	// File server serves static files over http.   http.Dir  points to the directory from which FileServer will serve the static file
+	// http.Handle maps the root url
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	// http.Handler is an intefrface that supports receiving and sending back requests over http, and  POST Is one of the requests
+
+	// ascii-art is the end poiint at  which backend handles the request sent by frontend.
 
 	http.HandleFunc("/ascii-art", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 			return
 		}
-
+		//
 		var request struct {
 			Banner string `json:"banner"`
 			Input  string `json:"input"`
 		}
+		// decoding the JavascriptObject Notaion Data
+		// r.Body is the request body of the incoming http reqrequest
+		// jso.NewDecoder  is a decoder taking only r.body as input, i.e taking only JSON data from  the request body
 
 		if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 			http.Error(w, "Invalid request body", http.StatusBadRequest)
 			return
 		}
-
+		// choosing the bannerfile to be mapped// the banner file selected by the user
 		fileName := asciiArt.BannerFile(request.Banner)
 
 		// Load the banner map from the file
@@ -47,7 +55,7 @@ func main() {
 	fmt.Println("Server is listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
-
+// logic for printing the asciii banner files in the required format
 func generateASCIIArt(input string, bannerMap map[int][]string) string {
 	lines := make([]string, 8)
 
